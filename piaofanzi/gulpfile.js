13be1f79fd58c8d18de68f76b733537a7b5a4ee1
aaron-fe <xiaohong4512@163.com>
2016-04-05 21:48:30 +0800

    PC

--- piaofanzi/gulpfile.js
+++ piaofanzi/gulpfile.js
@@ -6,13 +6,22 @@ var LessPluginCleanCSS = require('less-plugin-clean-css');
 var autoprefix = new LessPluginAutoPrefix({ browsers: ['last 2 versions'] });
 var cleancss = new LessPluginCleanCSS({ advanced: true });
 
-gulp.task('less-watch', function() {
+gulp.task('less-mobile', function() {
+	gulp.src('mobile/less/*.less')
+		.pipe(watch('mobile/less/*.less'))
+		.pipe(less({
+			plugins: [autoprefix, cleancss]
+		}))
+		.pipe(gulp.dest('mobile/css/'));
+});
+
+gulp.task('less-pc', function() {
 	gulp.src('less/*.less')
 		.pipe(watch('less/*.less'))
 		.pipe(less({
-			plugins: [autoprefix, cleancss]
+			plugins: [autoprefix]
 		}))
 		.pipe(gulp.dest('css/'));
 });
 
-gulp.task('default', ['less-watch']);
+gulp.task('default', ['less-mobile', 'less-pc']);
